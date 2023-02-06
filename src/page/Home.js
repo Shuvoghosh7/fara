457f42d05d1d587e09e4d5ab9f581b0d467b6d57
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-02-07 01:06:40 +0600

    add our story

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import OurStory from './OurStory/OurStory';
 
 
 const Home = () => {
     return (
         <div>
            <Banner/>
-            
+           <OurStory/>
         </div>
     );
 };
---
+++ src/page/OurStory/OurStory.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './OurStory.css'
+const OurStory = () => {
+    return (
+        <div className='ourStory'>
+            <p className='story-title'>In Your Dreams,Every Detail Matters</p>
+            <p className='story-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aut sit culpa porro sapiente ab consequatur explicabo nesciunt soluta et. Nisi beatae fugit rerum nemo, quibusdam ea qui est! Repudiandae.Nisi beatae fugit rerum nemo, quibusdam ea qui est! Repudiandae.quibusdam ea qui est! Repudiandae.Nisi beatae fugit rerum nemo, quibusdam ea qui est! Repudiandae Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aut sit culpa porro sapiente ab consequatur explicabo nesciunt soluta et. Nisi beatae fugit rerum nemo, quibusdam ea qui est! Repudiandae.Nisi beatae fugit rerum nemo, quibusdam ea qui est! Repudiandae.quibusdam ea qui est! Repudiandae.Nisi beatae fugit rerum nemo, quibusdam ea qui est! Repudiandae</p>
+            <div  className='story-btn'>
+                <button>Our Story</button>
+            </div>
+        </div>
+    );
+};
+
+export default OurStory;
