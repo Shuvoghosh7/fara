9d584b4f736e94ec823a59f5e202256adcca5df5
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-02-07 00:13:06 +0600

    complete banner part

---
+++ src/page/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Banner.css'
+const Banner = () => {
+    return (
+
+        <div className='banner'>
+            <div className="banner-contant">
+                <p className='banner-title1'>QUAM IM</p>
+                <p className='banner-title2'>MAKE YOUR SPECIAL BAY WITH US</p>
+               
+               <p className='banner-text'>
+                <span className='lg:ml-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quibusdam.adipisicing elit.</span><br />
+                <span className='lg:ml-24'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quibusdam . </span> <br />
+                <span className='lg:ml-48'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </span>
+               </p>
+
+            </div>
+        </div>
+
+    );
+};
+
+export default Banner;
