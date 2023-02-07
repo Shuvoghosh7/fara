129167215ebcfb8ad4c886b3f9c31b6b2bd365da
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-02-07 20:52:17 +0600

    fix service page css file

--- src/page/Services/Services.js
+++ src/page/Services/Services.js
@@ -11,25 +11,25 @@ const Services = () => {
             <div className='service-provide'>
                 <div>
                     <img src={Arbours} alt="" className='service-img'/>
-                    <p className='service-title'>Arbours</p>
+                    <p className='services-title'>Arbours</p>
                     <p className='service-about'>Lorem ipsum dolor sit amet consectetur .</p>
                     <p className='service-price'>Per Ddy <span>$100-$200</span></p>
                 </div>
                 <div>
                     <img src={Cake} alt="" className='service-img'/>
-                    <p className='service-title'>Cake Tables</p>
+                    <p className='services-title'>Cake Tables</p>
                     <p className='service-about'>Lorem ipsum dolor sit amet consectetur .</p>
-                    <p className='service-price'>Per Ddy <span>$100-$200</span></p>
+                    <p className='services-title'>Per Ddy <span>$100-$200</span></p>
                 </div>
                 <div>
                     <img src={Chair} alt="" className='service-img'/>
-                    <p className='service-title'>Chair</p>
+                    <p className='services-title'>Chair</p>
                     <p className='service-about'>Lorem ipsum dolor sit amet consectetur .</p>
                     <p className='service-price'>Per Ddy <span>$100-$200</span></p>
                 </div>
                 <div>
                 <img src={Event} alt="" className='service-img'/>
-                    <p className='service-title'>Event Props</p>
+                    <p className='services-title'>Event Props</p>
                     <p className='service-about'>Lorem ipsum dolor sit amet consectetur .</p>
                     <p className='service-price'>Per Ddy <span>$100-$200</span></p>
                 </div>
